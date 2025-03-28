6fd9493202c43a130453e5ed6a67887111289615
Justin Reynoso <justin.reynoso625@gmail.com>
2025-03-28 13:15:42 -0400

    Added about section

--- src/AboutSection.tsx
+++ src/AboutSection.tsx
@@ -1,10 +1,23 @@
+import image1 from "./images/Mario and Adrian A.jpg";
+import image2 from "./images/Mario and Adrian b.jpg";
+
 function AboutSection() {
-    return (
-        <section aria-labelledby="about-section-heading">
-            <h1 id="about-section-heading">About Section</h1>
-            <p>This is the About Section component.</p>
-        </section>
-    );
-};
+  return (
+    <section className="about-section">
+      <div>
+        <h1 className="title">Little Lemon</h1>
+        <h2 className="sub-title">Chicago</h2>
+        <p>
+          Little Lemon is a restaurant serving Mediterranean food with a modern
+          twist.
+        </p>
+      </div>
+      <div>
+        <img src={image1} alt="Mario and Adrian A" />
+        <img src={image2} alt="Mario and Adrian B" />
+      </div>
+    </section>
+  );
+}
 
 export default AboutSection;
