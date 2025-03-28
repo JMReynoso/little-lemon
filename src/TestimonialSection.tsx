acf7b8d198c98f50f287b50d42ea658a5b482899
Justin Reynoso <justin.reynoso625@gmail.com>
2025-03-28 13:41:30 -0400

    added variation in testimonial stars

--- src/TestimonialSection.tsx
+++ src/TestimonialSection.tsx
@@ -4,16 +4,31 @@ import person2 from "./images/people/3ca412176fe4306326b04a78b51fa148c49f99c1.pn
 import person3 from "./images/people/b64f988f077b50ffdab0afee9de4d701e4a9d5da.png";
 
 function TestimonialSection() {
-    return (
-        <section className="testimonial-section">
-            <h1 className="section-title">Testimonials</h1>
-            <div className="all-testimonial-cards">
-                <TestimonialCards rating={5} imagePath={person1} name="Jane Doe" review="Delicious and fresh." />
-                <TestimonialCards rating={5} imagePath={person2} name="Some Person" review="Delicious and fresh." />
-                <TestimonialCards rating={5} imagePath={person3} name="John Doe" review="Delicious and fresh." />
-            </div>
-        </section>
-    );
-};
+  return (
+    <section className="testimonial-section">
+      <h1 className="section-title">Testimonials</h1>
+      <div className="all-testimonial-cards">
+        <TestimonialCards
+          rating={5}
+          imagePath={person1}
+          name="Jane Doe"
+          review="Delicious and fresh."
+        />
+        <TestimonialCards
+          rating={3}
+          imagePath={person2}
+          name="Some Person"
+          review="Delicious and fresh."
+        />
+        <TestimonialCards
+          rating={4}
+          imagePath={person3}
+          name="John Doe"
+          review="Delicious and fresh."
+        />
+      </div>
+    </section>
+  );
+}
 
 export default TestimonialSection;
